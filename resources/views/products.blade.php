05bd71fe3b6c9d7a46548904fdf51b3afd5fe74b
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-07-08 01:44:42 +0200

    push finale

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($lista_prodotti as $prodotto)
-    <p>{{ $prodotto->type }}</p>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Prodotti</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>TIPO</th>
+                            <th>NOME PRODOTTO</th>
+                            <th>QUANTITA'</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($lista_prodotti as $prodotto)
+                            <tr>
+                                <td>{{ $prodotto->id }}</td>
+                                <td>{{ $prodotto->type }}</td>
+                                <td>{{ $prodotto->name_of_product }}</td>
+                                <td>{{ $prodotto->quantity }}</td>
+                            </tr>
+
+                        @endforeach
+                    </tbody>
+
+                </table>
+            </div>
+
+        </div>
+
+    </div>
 @endsection
 
 
