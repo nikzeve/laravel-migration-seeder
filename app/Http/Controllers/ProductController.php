1d52f28c16f91f5a342e44c18e5bdb58b28801e6
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-07-07 23:27:57 +0200

    push intermedia

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+        $prodotti = Product::all();
+        dd($prodotti);
+    }
 }
